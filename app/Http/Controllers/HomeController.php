c69f2aab7cd282adfc413491f986243845ef9560
fsepulveda1 <francisco.sepulveda30@gmail.com>
2020-08-02 19:49:50 -0400

    removing comments

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,11 +16,9 @@ class HomeController extends Controller
     /**
      * Create a new controller instance.
      *
-     * @return void
      */
     public function __construct()
     {
-        //$this->middleware('auth');
     }
 
     /**
@@ -30,8 +28,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        //$posts = Post::orderBy('id', 'desc')->simplePaginate(3);
-
         $posts = Tip::orderBy('id', 'desc')
                     ->get();
         $categories = Category::orderBy('orden', 'ASC')->get();
@@ -41,8 +37,6 @@ class HomeController extends Controller
 
     public function show($slug)
     {
-        //Find the post with the id = $id
-        //$post = Tip::find($slug);
         $post = Tip::where('slug', $slug)->first();
         $categories = Category::orderBy('orden', 'ASC')->get();
 
@@ -53,9 +47,8 @@ class HomeController extends Controller
     }
 
     public function buscar(Request $request){
-        //$q  = Input::get('q') ;
         $q  = $request->q;
-        //$products = DB::select("select * from products where nombre LIKE '%{$q}%'");
+
         $products = Product::with('colors', 'impresions', 'categories')->where('nombre', 'LIKE', "%{$q}%")->get();
 
         $categories = Category::orderBy('orden', 'ASC')->get();
