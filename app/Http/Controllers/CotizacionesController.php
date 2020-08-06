87a46234d983542c64af9b86519af4025f7a5b87
fsepulveda1 <francisco.sepulveda30@gmail.com>
2020-08-06 09:26:46 -0400

    custom browse controller

--- app/Http/Controllers/CotizacionesController.php
+++ app/Http/Controllers/CotizacionesController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use ReflectionClass;
@@ -13,28 +16,167 @@ use \TCG\Voyager\Events\BreadAdded;
 use \TCG\Voyager\Events\BreadDeleted;
 use \TCG\Voyager\Events\BreadUpdated;
 use \TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Http\Controllers\VoyagerBaseController;
 
-class CotizacionesController extends \TCG\Voyager\Http\Controllers\VoyagerBaseController
+class CotizacionesController extends VoyagerBaseController
 {
-    public function index()
+    public function index(Request $request)
     {
-        $this->authorize('browse_bread');
+        // GET THE SLUG, ex. 'posts', 'pages', etc.
+        $slug = $this->getSlug($request);
 
-        $dataTypes = Voyager::model('DataType')->select('id', 'name', 'slug')->get()->keyBy('name')->toArray();
+        // GET THE DataType based on the slug
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
 
-        $tables = array_map(function ($table) use ($dataTypes) {
-            $table = Str::replaceFirst(DB::getTablePrefix(), '', $table);
+        // Check permission
+        $this->authorize('browse', app($dataType->model_name));
 
-            $table = [
-                'prefix'     => DB::getTablePrefix(),
-                'name'       => $table,
-                'slug'       => $dataTypes[$table]['slug'] ?? null,
-                'dataTypeId' => $dataTypes[$table]['id'] ?? null,
-            ];
+        $getter = $dataType->server_side ? 'paginate' : 'get';
 
-            return (object) $table;
-        }, SchemaManager::listTableNames());
+        $searchNames = [
+            'clients.rut' => $request->get('rut'),
+            'clients.nombre' => $request->get('empresa'),
+            'clients.contacto' => $request->get('nombre'),
+            'clients.email' => $request->get('email'),
+            'cotizaciones.updated_at' => $request->get('fecha'),
+        ];
 
-        return Voyager::view('voyager::tools.bread.index')->with(compact('dataTypes', 'tables'));
+        $orderBy = $request->get('order_by', $dataType->order_column);
+        $sortOrder = $request->get('sort_order', $dataType->order_direction);
+        $usesSoftDeletes = false;
+        $showSoftDeleted = false;
+
+        // Next Get or Paginate the actual content from the MODEL that corresponds to the slug DataType
+        if (strlen($dataType->model_name) != 0) {
+            $model = app($dataType->model_name);
+
+            if ($dataType->scope && $dataType->scope != '' && method_exists($model, 'scope'.ucfirst($dataType->scope))) {
+                $query = $model->{$dataType->scope}();
+            } else {
+                $query = $model::select('cotizaciones.*');
+            }
+
+            // Use withTrashed() if model uses SoftDeletes and if toggle is selected
+            if ($model && in_array(SoftDeletes::class, class_uses_recursive($model)) && Auth::user()->can('delete', app($dataType->model_name))) {
+                $usesSoftDeletes = true;
+
+                if ($request->get('showSoftDeleted')) {
+                    $showSoftDeleted = true;
+                    $query = $query->withTrashed();
+                }
+            }
+            /** @var $query Builder */
+            $query->join('clients','cotizaciones.client_id','=','clients.id');
+
+            // If a column has a relationship associated with it, we do not want to show that field
+            $this->removeRelationshipField($dataType, 'browse');
+
+            foreach ($searchNames as $key => $value) {
+
+                if(empty($value))
+                    continue;
+
+                $search_value = '%'.$value.'%';
+                if($key == 'cotizaciones.updated_at') {
+                    $date = \DateTime::createFromFormat('m-d-Y',str_replace('/','-',substr($value,0,10)));
+                    $query->whereDate($key,'=', $date->format('Y-m-d'));
+                }
+                else {
+                    $query->where($key, 'LIKE', $search_value);
+                }
+
+            }
+
+            if ($orderBy && in_array($orderBy, $dataType->fields())) {
+                $querySortOrder = (!empty($sortOrder)) ? $sortOrder : 'desc';
+                $dataTypeContent = call_user_func([
+                    $query->orderBy($orderBy, $querySortOrder),
+                    $getter,
+                ]);
+            } elseif ($model->timestamps) {
+                $dataTypeContent = call_user_func([$query->latest($model::CREATED_AT), $getter]);
+            } else {
+                $dataTypeContent = call_user_func([$query->orderBy($model->getKeyName(), 'DESC'), $getter]);
+            }
+
+            // Replace relationships' keys for labels and create READ links if a slug is provided.
+            $dataTypeContent = $this->resolveRelations($dataTypeContent, $dataType);
+        } else {
+            // If Model doesn't exist, get data from table name
+            $dataTypeContent = call_user_func([DB::table($dataType->name), $getter]);
+            $model = false;
+        }
+
+        // Check if BREAD is Translatable
+        $isModelTranslatable = is_bread_translatable($model);
+
+        // Eagerload Relations
+        $this->eagerLoadRelations($dataTypeContent, $dataType, 'browse', $isModelTranslatable);
+
+        // Check if server side pagination is enabled
+        $isServerSide = isset($dataType->server_side) && $dataType->server_side;
+
+        // Check if a default search key is set
+        $defaultSearchKey = $dataType->default_search_key ?? null;
+
+        // Actions
+        $actions = [];
+        if (!empty($dataTypeContent->first())) {
+            foreach (Voyager::actions() as $action) {
+                $action = new $action($dataType, $dataTypeContent->first());
+
+                if ($action->shouldActionDisplayOnDataType()) {
+                    $actions[] = $action;
+                }
+            }
+        }
+
+        // Define showCheckboxColumn
+        $showCheckboxColumn = false;
+        if (Auth::user()->can('delete', app($dataType->model_name))) {
+            $showCheckboxColumn = true;
+        } else {
+            foreach ($actions as $action) {
+                if (method_exists($action, 'massAction')) {
+                    $showCheckboxColumn = true;
+                }
+            }
+        }
+
+        // Define orderColumn
+        $orderColumn = [];
+        if ($orderBy) {
+            $index = $dataType->browseRows->where('field', $orderBy)->keys()->first() + ($showCheckboxColumn ? 1 : 0);
+            $orderColumn = [[$index, $sortOrder ?? 'desc']];
+        }
+
+        $view = 'voyager::bread.browse';
+
+        if (view()->exists("voyager::$slug.browse")) {
+            $view = "voyager::$slug.browse";
+        }
+
+        return Voyager::view($view, compact(
+            'actions',
+            'dataType',
+            'dataTypeContent',
+            'isModelTranslatable',
+            'orderBy',
+            'orderColumn',
+            'sortOrder',
+            'searchNames',
+            'isServerSide',
+            'defaultSearchKey',
+            'usesSoftDeletes',
+            'showSoftDeleted',
+            'showCheckboxColumn'
+        ));
     }
 }
+
+
+
+
+
+
+
