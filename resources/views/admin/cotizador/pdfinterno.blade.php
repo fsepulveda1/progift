c7a45fb8f70dfb1cb0094d853650d1688b851f15
Francisco Seplveda Arias <francisco.sepulveda30@gmail.com>
2020-10-28 18:16:01 -0300

    fix spacing

--- resources/views/admin/cotizador/pdfinterno.blade.php
+++ resources/views/admin/cotizador/pdfinterno.blade.php
@@ -116,7 +116,7 @@
             </th>
         </tr>
     </table>
-    <table style='page-break-before:always'>
+    <table style='page-break-inside: auto !important;'>
     <table class="table w-100" border="1" cellpadding="0" cellspacing="0">
         <thead>
         <tr>
