d4779e279cfa1a7233ec3cfa89da30551742be50
Francisco Seplveda Arias <francisco.sepulveda30@gmail.com>
2020-11-05 23:04:01 -0300

    fix like query

--- app/Http/Controllers/Voyager/VoyagerMatchRutsController.php
+++ app/Http/Controllers/Voyager/VoyagerMatchRutsController.php
@@ -76,7 +76,7 @@ class VoyagerMatchRutsController extends VoyagerBaseController
             }
 
             if($filters['rut'])
-                $query->where('match_ruts.rut',"%".$filters['rut']."%",'LIKE');
+                $query->where('match_ruts.rut','LIKE',"%".$filters['rut']."%");
 
             if($filters['procedencia'])
                 $query->where('match_ruts.procedencia',$filters['procedencia']);
