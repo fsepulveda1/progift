d06e2559fc7db036fc4d44f6992ca043e1710af8
Francisco Seplveda Arias <francisco.sepulveda30@gmail.com>
2020-11-24 00:34:21 -0300

    adding loading in category page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -790,8 +790,7 @@
 
     $('.product-title').matchHeight();
 
-    if($('.product-ajax-grid').length === 0)
-        $('body').prepend('<div class="loading-overlay"><div class="bounce-loader"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div></div>');
+    $('body').prepend('<div class="loading-overlay"><div class="bounce-loader"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div></div>');
 
     //Variables
     var $loadButton = $('.loadmore .btn');
